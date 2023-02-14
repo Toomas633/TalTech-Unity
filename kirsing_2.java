789a647fa72ea14ad34805e38dd63daeafb0cc44
Toomas <toomas.kirsing@gmail.com>
2023-02-14 14:40:10 +0200

    Update kirsing_2.java

--- kirsing_2.java
+++ kirsing_2.java
@@ -1,8 +1,6 @@
-import org.junit.platform.console.shadow.picocli.CommandLine.ExitCode;
-
 public class kirsing_2 {
     public static void main(String[] args){
-        ül3(args);
+        ül2(args);
     }
     public static void ül1(String[] args){
         int[] arr={2,3,5,6};
