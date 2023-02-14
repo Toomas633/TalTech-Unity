63e90c381ffac8620929878f0ca0e1dac31b55b5
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-02-14 12:54:57 +0200

    Update kirsing_2.java

--- kirsing_2.java
+++ kirsing_2.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-
 public class kirsing_2 {
     public static void main(String[] args){
-        ül1(args);
+        ül3(args);
     }
     public static void ül1(String[] args){
         int[] arr={2,3,5,6};
@@ -18,5 +16,19 @@ public class kirsing_2 {
                 System.out.println(vastus);
             }
         }
+    }
+    public static void ül2(String[] args){
+        String es="kapsas", te="sibul";
+        String vastus = "";
+        if(es.charAt(es.length()-1) == te.charAt(0)){
+            vastus = es.substring(0,es.length()-1) + te.substring(1, te.length());
+        }
+        else{
+            vastus = es + te.substring(1, te.length());
+        }
+        System.out.println(vastus);
+    }
+    public static void ül3(String[] args){
+
     }
 }
