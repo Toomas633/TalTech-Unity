10b18a4e0ff5e198fbe1508ebd9b407de24f4179
Toomas <toomas.kirsing@gmail.com>
2023-02-14 15:59:15 +0200

    Update kirsing_kodu_1.java

--- kirsing_kodu_1.java
+++ kirsing_kodu_1.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class kirsing_kodu_1 {
     public static void main(String[] args){
         // vastav ülesanne mida käivitatakse ül1, ...2, ...3, ...4 vastavalt
-        ül3(args);
+        ül4(args);
     }
     public static void ül1(String[] args){
         String sisend="paaritu";
@@ -13,13 +15,13 @@ public class kirsing_kodu_1 {
         else {
             if(sisend.length() % 2 == 0){ //kas sõne pikkus on paaris või paaritu
                 keskmine = sisend.length()/2;
-                a = sisend.charAt(keskmine-1);
+                a = sisend.charAt(keskmine-1); //keskmine sümbol arvestades alguseks 0 mitte 1
                 b = sisend.charAt(keskmine);
                 System.out.println("Kaks keskmist sümbolit on: "+a+" "+b);
             }
             else{
                 keskmine = (sisend.length()-1)/2;
-                a = sisend.charAt(keskmine-1);
+                a = sisend.charAt(keskmine-1); //keskmine sümbol arvestades alguseks 0 mitte 1
                 b = sisend.charAt(keskmine);
                 c = sisend.charAt(keskmine+1);
                 System.out.println("Kolm keskmist sümbolit on: "+a+" "+b+" "+c);
@@ -40,9 +42,20 @@ public class kirsing_kodu_1 {
     public static void ül3(String[] args){
         int[] sisend={3,1,4,55,1,9,0};
         int otsitav=2, leitud=0;
-        for(int i=0; i<sisend.length;i++){
-            if(sisend[i] == otsitav) leitud += 1;
+        for(int i=0; i<sisend.length;i++){ //käi iga massiivi liikme kohta läbi
+            if(sisend[i] == otsitav) leitud += 1; //iga kord kui leitakse massiivist otsitav suurendatakse muutujat 1e võrra
         }
         System.out.println(leitud);
+    }
+    public static void ül4(String[] args){
+        String[] sisend ={"a","c","f","j","h"};
+        if(sisend.length<3) System.out.println("Sõne pole piisavalt pikk!"); //kontolli sõne pikkust
+        else{
+            String viimane = sisend[sisend.length-1]; //salvesta viimane ja kolmas koht mällu
+            String kolmas = sisend[sisend.length-3];
+            sisend[sisend.length-3] = viimane; //tee vastavad vahetused
+            sisend[sisend.length-1] = kolmas;
+            System.out.println("Vahetatud kohtadega massiiv on: "+ Arrays.toString(sisend));
+        }
     }
 }
