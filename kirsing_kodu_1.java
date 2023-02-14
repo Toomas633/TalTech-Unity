1e565606b4143b78370a6776d0da17f2c0ea6fad
Toomas <toomas.kirsing@gmail.com>
2023-02-14 15:07:55 +0200

    Update kirsing_kodu_1.java

--- kirsing_kodu_1.java
+++ kirsing_kodu_1.java
@@ -1,5 +1,29 @@
 public class kirsing_kodu_1 {
     public static void main(String[] args){
-        
+        // vastav ülesanne mida käivitatakse ül1, ...2, ...3, ...4 vastavalt
+        ül1(args);
+    }
+    public static void ül1(String[] args){
+        String sisend="paaritu";
+        char a, b, c;
+        int keskmine=0;
+        if(sisend.length() <= 1){
+            System.out.println("Sõne on: " + sisend);
+        }
+        else {
+            if(sisend.length() % 2 == 0){ //kas sõne pikkus on paaris või paaritu
+                keskmine = sisend.length()/2;
+                a = sisend.charAt(keskmine-1);
+                b = sisend.charAt(keskmine);
+                System.out.println("Kaks keskmist sümbolit on: "+a+" "+b);
+            }
+            else{
+                keskmine = (sisend.length()-1)/2;
+                a = sisend.charAt(keskmine-1);
+                b = sisend.charAt(keskmine);
+                c = sisend.charAt(keskmine+1);
+                System.out.println("Kolm keskmist sümbolit on: "+a+" "+b+" "+c);
+            }        
+        }
     }
 }
